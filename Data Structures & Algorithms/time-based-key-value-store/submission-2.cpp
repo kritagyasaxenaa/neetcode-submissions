455e875a00cf8254d969857fd8c5405ecc5679e1
kritagyasaxenaa <kritagyasaxenaa@gmail.com>
2026-05-01 17:04:06 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.cpp
@@ -0,0 +1,24 @@
+class TimeMap {
+public:
+    TimeMap(){
+
+    }
+    unordered_map<string,unordered_map<int, string>>m;
+    void set(string key, string value, int timestamp) {
+        m[key][timestamp]=value;
+        return;
+    }
+    
+    string get(string key, int timestamp) {
+            string s;
+            int i=timestamp;
+            while(i>0&&m[key][i].size()==0){
+                i--;
+            }
+            if(i!=0){
+                s=m[key][i];
+            }
+        
+         return s;
+    }
+};
