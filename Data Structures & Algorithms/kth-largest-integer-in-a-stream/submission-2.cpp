ac9ea87249492f273c398f9881ba5f4851aea8fb
kritagyasaxenaa <kritagyasaxenaa@gmail.com>
2026-06-16 11:34:51 +0530

    Add: kth-largest-integer-in-a-stream - submission-2

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-2.cpp
@@ -0,0 +1,19 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>>heap;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        // since one does not have to delete just store the top k elements in a min heap
+        this->k=k;
+        for(int i : nums){
+            heap.push(i);
+        }    
+    }
+    int add(int val) {
+        heap.push(val);
+        while(heap.size()>k){
+            heap.pop();
+        }
+        return heap.top();   
+    }
+};
